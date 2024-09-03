3d6aecd5832d0f4c15de8026e2b734b2475a59b4
Filipe Silva <cloudwalstorn@gmail.com>
2024-09-02 23:37:56 -0300

    implementação de mais testes de entidade

--- Backend/DevFreela.UnitTests/Core/Entities/ProjectTests.cs
+++ Backend/DevFreela.UnitTests/Core/Entities/ProjectTests.cs
@@ -1,10 +1,5 @@
 ﻿using DevFreela.Core.Entities;
 using DevFreela.Core.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DevFreela.UnitTests.Core.Entities
 {
@@ -28,6 +23,86 @@ namespace DevFreela.UnitTests.Core.Entities
 
             Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
             Assert.NotNull(project.StartedAt);
+        }
+
+        [Fact]
+        public void TestIfProjectCancelledWorks()
+        {
+            var project = new Project("Nome de teste", "Descrição de teste", 1, 2, 10000);
+
+            Assert.Equal(ProjectStatusEnum.Created, project.Status);
+            Assert.Null(project.StartedAt);
+
+            Assert.NotNull(project.Title);
+            Assert.NotEmpty(project.Title);
+
+            Assert.NotNull(project.Description);
+            Assert.NotEmpty(project.Description);
+
+            project.Start();
+
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+
+            project.Cancel();
+            Assert.Equal(ProjectStatusEnum.Cancelled, project.Status);
+        }
+
+        [Fact]
+        public void TestIfStatusProjectChangeforPaymentPendingWorks()
+        {
+            var project = new Project("Projeto Teste", "Descrição do Projeto de Teste", 1, 1, 20000);
+
+            Assert.Equal(ProjectStatusEnum.Created, project.Status);
+            Assert.Null(project.StartedAt);
+
+            Assert.NotNull(project.Title);
+            Assert.NotEmpty(project.Title);
+
+            Assert.NotNull(project.Description);
+            Assert.NotEmpty(project.Description);
+
+            Assert.True(project.IdClient > 0);
+            Assert.True(project.IdFreelancer > 0);
+
+            Assert.NotNull(project.Description);
+            Assert.NotEmpty(project.Description);
+
+            project.SetPaymentPending();
+
+            Assert.Equal(ProjectStatusEnum.PaymentPending, project.Status);
+            Assert.Null(project.FinishedAt);
+        }
+
+        [Fact]
+        public void TestIfStatusProjectFinishWorks()
+        {
+            var project = new Project("Projeto Teste", "Descrição do Projeto de Teste", 1, 1, 20000);
+
+            Assert.Equal(ProjectStatusEnum.Created, project.Status);
+            Assert.Null(project.StartedAt);
+
+            Assert.NotNull(project.Title);
+            Assert.NotEmpty(project.Title);
+
+            Assert.NotNull(project.Description);
+            Assert.NotEmpty(project.Description);
+
+            Assert.True(project.IdClient > 0);
+            Assert.True(project.IdFreelancer > 0);
+
+            Assert.NotNull(project.Description);
+            Assert.NotEmpty(project.Description);
+
+            project.SetPaymentPending();
+
+            Assert.Equal(ProjectStatusEnum.PaymentPending, project.Status);
+            Assert.Null(project.FinishedAt);
+
+            project.Finish();
+
+            Assert.Equal(ProjectStatusEnum.Finished, project.Status);
+            Assert.NotNull(project.FinishedAt);
         }
     }
 }
---
+++ Backend/DevFreela.UnitTests/Core/Entities/UserTests.cs
@@ -0,0 +1,36 @@
+﻿using DevFreela.Core.Entities;
+
+namespace DevFreela.UnitTests.Core.Entities
+{
+    public class UserTests
+    {
+        [Fact]
+        public void TestICreateActiveUserWorks()
+        {
+            var user = new User("User Teste", "teste@teste.com", DateTime.Now, "Aa12345!", "client");
+            Assert.NotNull(user.FullName);
+            Assert.NotEmpty(user.FullName);
+
+            Assert.NotNull(user.Email);
+            Assert.NotEmpty(user.Email);
+
+            Assert.True(user.Active);
+        }
+
+        [Fact]
+        public void TestIfDisableUserWorks()
+        {
+            var user = new User("User Teste", "teste@teste.com", DateTime.Now, "Aa12345!", "client");
+            Assert.NotNull(user.FullName);
+            Assert.NotEmpty(user.FullName);
+
+            Assert.NotNull(user.Email);
+            Assert.NotEmpty(user.Email);
+
+            Assert.True(user.Active);
+
+            user.Delete();
+            Assert.True(!user.Active);
+        }
+    }
+}
